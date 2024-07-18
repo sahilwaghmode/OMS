4daaaaecba8ec789490e9a7bb4505ff1ed29c910
Sahil Waghmode <sahilwaghmode@gmail.com>
2024-07-19 00:00:06 +0530

    Fixed Repo Struct

---
+++ OrderBook/OrderBook/CSVReader.hpp
@@ -0,0 +1,25 @@
+//
+//  CSVReader.hpp
+//  OrderBook
+//
+//  Created by Sahil Waghmode on 18/07/24.
+//
+
+#ifndef CSVReader_hpp
+#define CSVReader_hpp
+
+#include <string>
+#include <fstream>
+
+class CSVReader
+{
+public:
+    CSVReader(const std::string& filename);
+
+    std::vector<std::vector<std::string>> readCSV();
+
+private:
+    std::ifstream file;
+};
+
+#endif /* CSVReader_hpp */
---
+++ OrderBook/OrderBook/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  OrderBook
+//
+//  Created by Sahil Waghmode on 18/07/24.
+//
+
+#include <iostream>
+
+#include "CSVReader.hpp"
+#include <vector>
+
+int main(int argc, const char * argv[])
+{
+    CSVReader reader("/Users/sahilwaghmode/CppDesigns/OMS/OrderBook/OrderBookDataSet/AMZN_message.csv");
+    
+    auto start_time = std::chrono::high_resolution_clock::now();
+    std::vector<std::vector<std::string>> data = reader.readCSV();
+    auto end_time = std::chrono::high_resolution_clock::now();
+    
+    auto readingTime = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+    
+    std::cout << "File read in " << readingTime << " ms\n";
+    return 0;
+}
